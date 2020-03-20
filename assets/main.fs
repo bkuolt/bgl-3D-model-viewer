1fd355b37e6a4e15564c2501c5bb36184025204a
Bastian Kuolt <bastian.kuolt@gmail.com>
2020-03-20 11:20:12 +0100

    updated shaders

--- assets/main.fs
+++ assets/main.fs
@@ -1,20 +1,26 @@
 // Copyright 2020 Bastian Kuolt
-#version 150 core  // gl_FragColor
+#version 330 core  // gl_FragColor
+#extension GL_ARB_explicit_uniform_location : require
 
-struct Pixel {
-    vec3 normal;
+struct Light {
+    vec3 direction;
+    vec3 color;
 };
 
-// layout (location = 4) uniform sampler2D texture;
-in Pixel pixel;  /* not used yet */
+layout (location = 4) uniform sampler2D texture;  /* not supported yet */
+layout (location = 5) uniform Light light;
+
+in vec3 N;
 
 float calculateLightIntensity() {
+    // TODO(bkuolt): use data from light struct
     const vec3 L = vec3(-1.0f, -1.0f, -1.0f);
-    return max(dot(L, normalize(pixel.normal)), 0.0);
+    return max(dot(L, normalize(N)), 0.0);
 }
 
 void main() {
     const vec4 ambient = vec4(0.3f, 0.3f, 0.3f, 1.0f);
     const vec4 lightColor = vec4(0.5, 0.5, 0.0, 1.0);
+    // TODO(bkuolt): use data from light struct
     gl_FragColor = ambient + (lightColor * calculateLightIntensity());
 }
--- assets/main.vs
+++ assets/main.vs
@@ -1,19 +1,21 @@
 // Copyright 2020 Bastian Kuolt
 #version 450 core
 
+struct Light {
+    vec3 direction;
+    vec3 color;
+};
+
 layout (location = 0) uniform mat4 MVP;
 layout (location = 1) in vec3 position;
 layout (location = 2) in vec3 normal;
 layout (location = 3) in vec2 texcoords;
-layout (location = 4) uniform sampler2D texture;  /* not used yet */
+layout (location = 4) uniform sampler2D texture;  /* not supported yet */
+layout (location = 5) uniform Light light;
 
-struct Pixel {
-    vec3 normal;
-};
-
-out Pixel pixel;  /* not used yet */
+out vec3 N;
 
 void main() {
     gl_Position = MVP * vec4(position, 1.0);
-    pixel.normal = normalize(mat3(MVP) * normal);
+    N = normalize(mat3(MVP) * normal);
 }
